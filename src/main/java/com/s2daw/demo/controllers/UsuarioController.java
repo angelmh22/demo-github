91ea58a29104c8dc27aa64fe04f8f326ab8e1efb
angelmh22 <angelmartinezhu@gmail.com>
2023-02-11 12:36:56 +0100

    Hasta minuto 01:25:55 del curso (crea Usuario y lo devuelve pasando el id, pero sin acceder todavía a la BBDD)

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.s2daw.demo.controllers;
 
+import com.s2daw.demo.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,44 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value="prueba")
-    public List<String> prueba(){
-        return List.of("prueba","prueba2","prueba3");
+    @RequestMapping(value="usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id) {
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Ángel");
+        usuario.setApellido("Martínez");
+        usuario.setEmail("angelmartinezhu@gmail.com");
+        usuario.setTelefono("673415718");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario12")
+    public Usuario editar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Ángel");
+        usuario.setApellido("Martínez");
+        usuario.setEmail("angelmartinezhu@gmail.com");
+        usuario.setTelefono("673415718");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario22")
+    public Usuario eliminar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Ángel");
+        usuario.setApellido("Martínez");
+        usuario.setEmail("angelmartinezhu@gmail.com");
+        usuario.setTelefono("673415718");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario32")
+    public Usuario buscar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Ángel");
+        usuario.setApellido("Martínez");
+        usuario.setEmail("angelmartinezhu@gmail.com");
+        usuario.setTelefono("673415718");
+        return usuario;
     }
 }
