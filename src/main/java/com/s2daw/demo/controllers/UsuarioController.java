b6a63fdadbba60145349b9896183ff09b7c63203
angelmh22 <angelmartinezhu@gmail.com>
2023-02-13 20:57:44 +0100

    Hasta minuto 02:39:44 del curso (se añade la opción para borrar un usuario y en las rutas del backend se añade /api)

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -5,8 +5,10 @@ import com.s2daw.demo.models.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +18,7 @@ public class UsuarioController {
     @Autowired
     private UsuarioDao usuarioDao;
 
-    @RequestMapping(value="usuario/{id}")
+    @RequestMapping(value="api/usuarios/{id}", method = RequestMethod.GET)
     public Usuario getUsuario(@PathVariable Long id) {
         Usuario usuario = new Usuario();
         usuario.setId(id);
@@ -27,7 +29,7 @@ public class UsuarioController {
         return usuario;
     }
 
-    @RequestMapping(value="usuarios")
+    @RequestMapping(value="api/usuarios")
     public List<Usuario> getUsuarios() {
         return usuarioDao.getUsuarios();
 
@@ -43,14 +45,9 @@ public class UsuarioController {
         return usuario;
     }
 
-    @RequestMapping(value="usuario22")
-    public Usuario eliminar() {
-        Usuario usuario = new Usuario();
-        usuario.setNombre("Ángel");
-        usuario.setApellido("Martínez");
-        usuario.setEmail("angelmartinezhu@gmail.com");
-        usuario.setTelefono("673415718");
-        return usuario;
+    @RequestMapping(value="api/usuarios/{id}", method = RequestMethod.DELETE)
+    public void eliminar(@PathVariable Long id) {
+        usuarioDao.eliminar(id);
     }
 
     @RequestMapping(value="usuario32")
