6a12fa1447788e98087910865d33a855dbcb0015
angelmh22 <angelmartinezhu@gmail.com>
2023-02-13 18:14:14 +0100

    Hasta minuto 01:55:18 del curso (backend devuelve la lista de usuarios sin acceder a la BBDD, ya que el fronted devuelve la lista de usuarios en una tabla)

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,6 +20,36 @@ public class UsuarioController {
         usuario.setEmail("angelmartinezhu@gmail.com");
         usuario.setTelefono("673415718");
         return usuario;
+    }
+
+    @RequestMapping(value="usuarios")
+    public List<Usuario> getUsuarios() {
+        List<Usuario> usuarios = new ArrayList<>();
+        Usuario usuario = new Usuario();
+        usuario.setId(111L);
+        usuario.setNombre("Ángel");
+        usuario.setApellido("Martínez");
+        usuario.setEmail("angelmartinezhu@gmail.com");
+        usuario.setTelefono("673415718");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(222L);
+        usuario2.setNombre("Sheila");
+        usuario2.setApellido("Corrales");
+        usuario2.setEmail("sheilacgarcia@gmail.com");
+        usuario2.setTelefono("684356722");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(333L);
+        usuario3.setNombre("Alvaro");
+        usuario3.setApellido("Adanez");
+        usuario3.setEmail("alvaroah15@gmail.com");
+        usuario3.setTelefono("693465238");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+        return usuarios;
     }
 
     @RequestMapping(value="usuario12")
