3f4f287524ab266050744d2ec063b75c82a988c8
angelmh22 <angelmartinezhu@gmail.com>
2023-02-13 17:27:03 +0100

    Hasta minuto 01:41:32 del curso (modifica usuarios.js para que consuma backend mediante fetch, pero al no haber BBDD no devuelve datos de ella)

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -1,4 +1,19 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
+    cargarUsuarios();
   $('#usuarios').DataTable();
 });
+
+async function cargarUsuarios() {
+
+      const request = await fetch('usuario/2222', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+      const usuarios = await request.json();
+
+      console.log(usuarios);
+}
